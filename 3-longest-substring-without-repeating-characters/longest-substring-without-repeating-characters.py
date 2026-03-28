fdb4160f620cc86cafd2314f17087cf17d2dbf11
Kaushik <107872261+kaushikar11@users.noreply.github.com>
2026-03-28 15:53:40 -0400

    Time: 19 ms (22.99%) | Memory: 19.3 MB (82.65%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,33 +1,14 @@
-# class Solution:
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         maxLen = 0
-        
-#         for i in range(0,len(s)):
-#             h={}
-#             for j in range(i,len(s)):
-#                 if s[j] in h:
-#                     break
-#                     if maxLen<len(h):
-#                         maxLen = len(h)
-#                     h={}
-#                 h[s[j]] = 1
-#                 maxLen = max(maxLen,len(h))
-              
-#         return maxLen
-
-
-        
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        seen = set()
+        subset = set()
         left = 0
         longest = 0
-        for right, char in enumerate(s):
-            while char in seen:
-                seen.remove(s[left])
+
+        for right in range(len(s)):
+            while s[right] in subset:
+                subset.remove(s[left])
                 left+=1
-            seen.add(s[right])
-            longest = max(longest, right-left+1)
+            subset.add(s[right])
+            longest = max(longest, len(s[left:right])+1)
         return longest
-                
 
