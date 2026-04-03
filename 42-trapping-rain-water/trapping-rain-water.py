92b9aecc6affd0fb7cb255600aca012091c507ed
Kaushik <107872261+kaushikar11@users.noreply.github.com>
2026-04-03 16:25:47 -0400

    Time: 16 ms (22.44%) | Memory: 21 MB (90.19%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -42,25 +42,3 @@ class Solution:
 
 
 
-
-
-
-
-
-        # if not height:
-        #     return 0
-        # n = len(height)
-        # pre = [0]*n
-        # post = [0]*n
-        # water = 0
-        # pre[0] = height[0]
-        # post[n-1] = height[n-1]
-
-        # for i in range(0,n):
-        #     pre[i] = max(pre[i-1], height[i])
-        # for i in range(n-2,-1,-1):
-        #     post[i] = max(post[i+1], height[i])
-
-        # for i in range(n):
-        #     water += min(pre[i],post[i]) - height[i]
-        # return water
