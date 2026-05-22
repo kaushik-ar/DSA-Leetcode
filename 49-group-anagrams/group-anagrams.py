00da04f53802ec6bb79e42f07703ae1f0d2ee4f0
Kaushik <107872261+kaushik-ar@users.noreply.github.com>
2026-05-22 14:50:25 -0600

    Time: 11 ms (84.33%) | Memory: 22 MB (67.43%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -3,13 +3,11 @@ from collections import defaultdict
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashmap = defaultdict(list)
-        result = []
         for string in strs:
             key = ''.join(sorted(string.lower()))
             hashmap[key].append(string)
-        for key in hashmap:
-            result.append(hashmap[key])
-        return result
+        return list(hashmap.values())
+        
 
 
         
