60eced668d5c41a7ea47c4eacf7b03a77b0ead85
Kaushik <107872261+kaushik-ar@users.noreply.github.com>
2026-05-25 14:38:19 -0600

    Time: 775 ms (19.92%) | Memory: 26.4 MB (81.46%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,35 +1,34 @@
-import random
 
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def merge(arr, l, m, r):
-            left, right = arr[l:m+1], arr[m+1:r+1]
-            i, j, k = l, 0, 0
+        def mergeSort(arr, l, r):
+            if l>=r:
+                return arr
 
+            mid = (l+r)//2
+            mergeSort(arr, l, mid)
+            mergeSort(arr, mid+1, r)
+            merge(arr, l, mid, r)
+            return arr
+
+        def merge(arr, l, mid, r):
+            left, right = arr[l:mid+1], arr[mid+1:r+1]
+            i, j, k = l, 0, 0
             while j< len(left) and k < len(right):
-                if left[j] <= right[k]:
+                if left[j]<= right[k]:
                     arr[i] = left[j]
                     j+=1
                 else:
                     arr[i] = right[k]
                     k+=1
                 i+=1
-            while j < len(left):
-                arr[i] = left[j]
+            while j< len(left):
+                arr[i]= left[j]
                 j+=1
                 i+=1
-            while k < len(right):
+            while k<len(right):
                 arr[i] = right[k]
                 k+=1
                 i+=1
-
-        def mergeSort(arr, l, r):
-            if l == r:
-                return arr
-
-            m = (l+r)//2
-            mergeSort(arr, l, m)
-            mergeSort(arr, m+1, r)
-            merge(arr, l, m, r)
-            return arr
         return mergeSort(nums, 0, len(nums)-1)
+            
